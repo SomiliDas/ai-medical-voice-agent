28e6f4abdd4b31429fb54e48fad912aeea206836
SomiliDas <somili.das12@gmail.com>
2026-07-24 09:44:06 +0530

    selectedDoctor method updated

--- app/(routes)/dashboard/_components/SuggestedDoctorCard.tsx
+++ app/(routes)/dashboard/_components/SuggestedDoctorCard.tsx
@@ -6,14 +6,16 @@ import Image from 'next/image'
 
 type props = {
     doctorAgent : doctorAgent,
-    setSelectedDoctor : doctorAgent,
+    setSelectedDoctor : React.Dispatch<
+    React.SetStateAction<doctorAgent | undefined>
+  > ,
     selectedDoctor : doctorAgent
 }
 
 
 function SuggestedDoctorCard({doctorAgent, setSelectedDoctor, selectedDoctor} : props) {
   return (
-    <div className={`flex flex-col items-center gap-1 p-4 border rounded-2xl shadow cursor-pointer hover:border-blue-500 ${selectedDoctor?.id ==doctorAgent.id && `border-blue-500` }`} onClick={()=>setSelectedDoctor(doctorAgent)}>
+    <div className={`flex flex-col items-center gap-1 p-4 border rounded-2xl shadow cursor-pointer hover:border-blue-500 ${selectedDoctor?.id ==doctorAgent.id && `border-blue-500` }`} onClick={()=>(setSelectedDoctor(doctorAgent))}>
         <Image src={doctorAgent.image} alt={doctorAgent.specialist} width={70} height={70} className=' w-[50px] h-[50px] rounded-4xl object-cover mt-2'/>
         <h2 className='font-semibold text-sm text-center'>{doctorAgent.specialist}</h2>
         <p className='text-sm text-center'>{doctorAgent.description}</p>
